3fb143d40b82d531a2e8b0a1d29f4247336c53c3
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-03-09 21:49:50 +0800

    style: change font size

--- Project3_PigGame/index.js
+++ Project3_PigGame/index.js
@@ -1,8 +1,8 @@
 'use-static';
 
 // Selecting elements by id
-const score0EL = document.getElementById('#score--0');
-const score1EL = document.getElementById('score--1');
+const score0EL = document.getElementById('#score-0');
+const score1EL = document.getElementById('#score-1');
 
 score0EL.textContent = 0;
 score1EL.textContent = 0;
