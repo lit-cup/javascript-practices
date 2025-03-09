e7b86f215b6860e4e4e994239502aa9123d60dc7
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-03-09 16:36:02 +0800

    refactor: create funtion to handle open/close modal actually do

--- Project2_ModalWindow/index.js
+++ Project2_ModalWindow/index.js
@@ -5,17 +5,17 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.open-modal'); //<- this is Array
 
-for(let i=0; i<btnsOpenModal.length; i++){
-    btnsOpenModal[i].addEventListener('click', function(){
-        modal.classList.remove('content');
-        overlay.classList.remove('content');
-    });
+const openModal = function(){
+    modal.classList.remove('content');
+    overlay.classList.remove('content');
 }
-btnCloseModal.addEventListener('click', function(){
+const closeModal = function(){
     modal.classList.add('content');
     overlay.classList.add('content');
-});
-overlay.addEventListener('click', function(){
-    modal.classList.add('content');
-    overlay.classList.add('content');
-});
+}
+
+for(let i=0; i<btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
