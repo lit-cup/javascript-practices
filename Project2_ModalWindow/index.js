3cbcffcd9575e7ca4bdb6a3504ffcda4a6483563
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-03-09 16:28:54 +0800

    feat: Cerate simple page and add open action modal

---
+++ Project2_ModalWindow/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.open-modal'); //<- this is Array
+
+for(let i=0; i<btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', function(){
+        modal.classList.remove('content');
+        overlay.classList.remove('content');
+    });
+}
