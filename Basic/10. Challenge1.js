d5711bed9dd10d73f2ad7f0d7713570911898c34
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-03-21 15:21:14 +0800

    style: fix some logical

--- Basic/10. Challenge1.js
+++ Basic/10. Challenge1.js
@@ -53,22 +53,26 @@ const game ={
 
 // 1. Create one player array for each team (variables `players1` and `players2`)  
 const [players1, players2] = game.players;
+console.log(players1, players2);
 
 // 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) 
 //    create one variable (`gk`) with the goalkeeper's name, and one array (`fieldPlayers`) with all the remaining 10 field players  
 const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
 
 // 3. Create an array `allPlayers` containing all players of both teams (22 players)  
 const allPlayers = [...players1, ...players2];
-
+console.log(allPlayers);
 
 // 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array (`players1Final`) containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'  
 const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+console.log(players1Final);
 
 // 5. Based on the `game.odds` object, create one variable for each odd (called `team1`, `draw` and `team2`) 
 const {
     odds: {team1, x: draw, team2}
 } = game;
+console.log(team1, draw, team2);
 
 // 6. Write a function (`printGoals`) that receives an arbitrary number of player names (NOT an array) 
 //    and prints each of them to the console, along with the number of goals who were scored (number of player names passed in)  
