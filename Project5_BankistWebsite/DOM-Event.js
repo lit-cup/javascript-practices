8505fb78b79e38dc8f511dfb254840532748aaf4
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-04-24 15:20:06 +0800

    feat: add DOM Traversing note

--- Project5_BankistWebsite/DOM-Event.js
+++ Project5_BankistWebsite/DOM-Event.js
@@ -149,3 +149,32 @@ document.querySelector('.nav').addEventListener('click', function (e) {
     console.log('NAV', e.target, e.currentTarget);
     console.log(e.currentTarget === this);
 })
+
+
+// DOM Traversing
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+// all sibling and not just previous and the next one
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+    if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+
+// 
