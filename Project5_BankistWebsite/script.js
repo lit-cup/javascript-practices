d716bcb92ba20945d435a05f708ba5afe9012379
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-04-24 15:37:08 +0800

    feat: Menu fade animation - Passing Arguments to Event Handler by bind()

--- Project5_BankistWebsite/script.js
+++ Project5_BankistWebsite/script.js
@@ -8,6 +8,11 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav');
 ///////////////////////////////////////
 // Modal window
 
@@ -87,15 +92,11 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   // Matching strategy
   if (e.target.classList.contains('nav__link')) {
     const sectionId = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
 // Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
@@ -113,3 +114,20 @@ tabsContainer.addEventListener('click', function (e) {
   document.querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const headFadeHover = function (e) {
+  if (e.target.classList.contains('nav__link'));
+  const link = e.target;
+  const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+  const logo = link.closest('.nav').querySelector('.img');
+
+  siblings.forEach(el => {
+    if (el !== link) el.style.opacity = this;
+  });
+  logo.style.opacity = this;
+}
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', headFadeHover.bind(0.5));
+nav.addEventListener('mouseout', headFadeHover.bind(1));
