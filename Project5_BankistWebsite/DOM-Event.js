1cfff7d07f4cda7bf2bdf9013812b925239e63bd
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-04-24 12:14:52 +0800

    feat: add noteEvent handlers - addEventListener(), removeEventListener() - Event Propagation: capturing, target, and bubbling phases

--- Project5_BankistWebsite/DOM-Event.js
+++ Project5_BankistWebsite/DOM-Event.js
@@ -94,3 +94,58 @@ logo.classList.contains('c');
 
 // Don't use
 logo.className = 'jonas';
+
+
+// Types if events/ Event Handlers
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+    alert('addEventListener: Great! You are reading the heding :D');
+}
+
+h1.addEventListener('mouseenter', alertH1);
+
+// remove after 3 sec
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000)
+// h1.onmouseenter = function (e) {
+//     alert('addEventListener: Great! You are reading the heding :D');
+// }
+
+// Event Propagation: Understanding how events propagate through the DOM
+// Events propagate in three phases: capturing, target, and bubbling phases
+
+// Example: Adding event listeners to demonstrate event propagation by color
+const randomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+    `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+{/* <nav class="nav">
+      <ul class="nav__links">
+        <li class="nav__item">
+          <a class="nav__link" href="#section--1">Features</a> */}
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+    // Case 1 click nav__link element
+    // all have click listener will active because of Bubbling(parent element)
+    this.style.backgroundColor = randomColor();
+    // selct target and currentTarget are different
+    console.log('Link', e.target, e.currentTarget);
+    // currentTarget are same as 'this' attach
+    console.log(e.currentTarget === this);
+
+    // // Stop propagation
+    // e.stopPropagation();
+})
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('CONTAINER', e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+})
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+})
