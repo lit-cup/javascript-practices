4803577ad01aa3f2cac8097bfe66268bea7c510d
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-04-29 16:45:59 +0800

    feat: add prototype chain usage

--- Basic/OOP/jsOOP.js
+++ Basic/OOP/jsOOP.js
@@ -60,3 +60,27 @@ console.log(john.species); // Human inherited from Person.prototype
 
 console.log(chen.hasOwnProperty('name')); // true
 console.log(chen.hasOwnProperty('species')); // false because species is on the prototype not chen object prototype
+
+// protoype chain
+console.log(chen.__proto__); // Person { constructor: [Function: Person], calcAge: [Function] }
+console.log(chen.__proto__.__proto__); // Object { constructor: [Function: Object], __defineGetter__: [Function], __defineSetter__: [Function], hasOwnProperty: [Function], __lookupGetter__: [Function], ... }
+console.log(chen.__proto__.__proto__.__proto__); // null
+
+console.dir(chen.__proto__.constructor); // Person { constructor: [Function: Person], calcAge: [Function] }
+
+const arr = [1, 2, 3, 4, 5, 1, 2, 1, 1];    // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype); // true
+
+console.log(arr.__proto__.__proto__); // Object { constructor: [Function: Object], __defineGetter__: [Function], __defineSetter__: [Function], hasOwnProperty: [Function], __lookupGetter__: [Function], ... }
+
+// Example of a prototype chain
+// creating a new method on the Array prototype(top)
+Array.prototype.unique = function () {
+    return [...new Set(this)]; // using set to remove duplicates
+}
+// calling the new method on the array instance and it works with set() method
+console.log(arr.unique()); // [ 1, 2, 3, 4, 5 ]
+
+const h1 = document.querySelector('h1'); // 6th layers of the prototype chain to Object
+console.dir(x => x + 1);  // 5th layers of the prototype chain to Object
