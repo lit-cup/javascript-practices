8b7dc1982ab11866accb5046878ba688c70f895b
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-05-06 21:48:33 +0800

    feat: add _setDescription()、_renderWorkout()、_hideform() - model description title - renderWorkout form - hideform to handle after add event

--- Project6_Mapty/script.js
+++ Project6_Mapty/script.js
@@ -20,12 +20,19 @@ class Workout {
         this.distance = distance; // in km
         this.duration = duration; // in min
     }
+    _setDescription() {
+        // prettier-ignore
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
 }
 class Running extends Workout {
     type = 'running';
     constructor(coords, distance, duration, cadence) {
         super(coords, distance, duration);
         this.cadence = cadence; // in steps/min
+        this.calcPace();
+        this._setDescription();
     }
     calcPace() {
         this.pace = this.duration / this.distance; // in min/km
@@ -38,6 +45,8 @@ class Cycling extends Workout {
     constructor(coords, distance, duration, elevationGain) {
         super(coords, distance, duration);
         this.elevationGain = elevationGain; // in m
+        this.calcSpeed();
+        this._setDescription();
     }
     calcSpeed() {
         this.speed = this.distance / (this.duration / 60); // in km/h
@@ -103,7 +112,7 @@ class maptyApp {
                     className: `${workout.type}-popup`
                 })
             )
-            .setPopupContent(`${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.distance}`)
+            .setPopupContent(`${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`)
             .openPopup();
     }
     _showForm(mapE) {
@@ -153,34 +162,73 @@ class maptyApp {
         }
 
         // Add new object to the workouts array
-        console.log(workout);
+        console.log(workout.pace);
         this.#workouts.push(workout);
 
-        // // Render workout on the list
-        // this._renderWorkout(workout);
+        // Render workout on the list
+        this._renderWorkout(workout);
 
-        // clear the input fields
-        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+        // clear the input fields hide the form
+        this._hideForm();
 
         // display the form
         this._pinMap(workout);
     }
-    // _renderWorkout(workout) {
-    //     const html = `
-    //     <li class="workout workout--${workout.name}" data-id="${workout.id}">
-    //       <h2 class="workout__title">Running on April 14</h2>
-    //       <div class="workout__details">
-    //         <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'}</span>
-    //         <span class="workout__value">${workout.distance}</span>
-    //         <span class="workout__unit">km</span>
-    //       </div>
-    //       <div class="workout__details">
-    //         <span class="workout__icon">⏱</span>
-    //         <span class="workout__value">${workout.duration}</span>
-    //         <span class="workout__unit">min</span>
-    //       </div>
-    //     `;
-    // }
+    _hideForm() {
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+        form.style.display = 'none';
+        form.classList.add('hidden');
+        setTimeout(() => (form.style.display = 'grid'), 1000);
+    }
+    _renderWorkout(workout) {
+        let html = `
+        <li class="workout workout--${workout.type}" data-id="${workout.id}">
+          <h2 class="workout__title">${workout.description}</h2>
+          <div class="workout__details">
+            <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'}</span>
+            <span class="workout__value">${workout.distance}</span>
+            <span class="workout__unit">km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⏱</span>
+            <span class="workout__value">${workout.duration}</span>
+            <span class="workout__unit">min</span>
+          </div>
+        `;
+        if (workout.type === 'running') {
+            html += `
+            <div class="workout__details">
+              <span class="workout__icon">⚡️</span>
+              <span class="workout__value">${workout.pace.toFixed(1)}</span>
+              <span class="workout__unit">min/km</span>
+            </div>
+            <div class="workout__details">
+              <span class="workout__icon">🦶🏼</span>
+              <span class="workout__value">${workout.cadence}</span>
+              <span class="workout__unit">spm</span>
+            </div>
+          </li>`;
+        }
+
+        if (workout.type === 'cycling') {
+            html += `
+            <div class="workout__details">
+              <span class="workout__icon">⚡️</span>
+              <span class="workout__value">${workout.speed.toFixed(1)}</span>
+              <span class="workout__unit">km/h</span>
+            </div>
+            <div class="workout__details">
+              <span class="workout__icon">⛰</span>
+              <span class="workout__value">${workout.elevationGain}</span>
+              <span class="workout__unit">m</span>
+            </div>
+          </li>`;
+
+
+        }
+        form.insertAdjacentHTML('afterend', html);
+    }
 }
 
 const app = new maptyApp();
