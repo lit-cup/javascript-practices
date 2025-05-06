d474ec5d01258af4bfa2e98ca7c8b83c6dcd3d25
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-05-06 17:01:05 +0800

    refactor: Mapty to maptyApp class - Private map, mapEvent - _getPosition() - _loadMap() - _pinMap(map, mapEvent) - _showForm(mapE) - _toggleElevationField() - _newWorkout(e)

--- Project6_Mapty/script.js
+++ Project6_Mapty/script.js
@@ -11,83 +11,80 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
 
+class maptyApp {
 
-const pinMap = (map, mapEvent) => {
-    const { lat, lng } = mapEvent.latlng;
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-            L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: 'running-popup'
-            })
-        )
-        .setPopupContent('Workout')
-        .openPopup();
-}
+    #map;
+    #mapEvent;
 
+    constructor() {
+        this._getPosition();
+        // using bind because the private variable is not accessible in the event handler
+        form.addEventListener('submit', this._newWorkout.bind(this));
 
-const renderMap = (latitude, longitude) => {
-    const coords = [latitude, longitude];
-    map = L.map('map').setView(coords, 13);
+        inputType.addEventListener('change', this._toggleElevationField);
+    }
+    _getPosition() {
+        if (navigator.geolocation) {
+            console.log('Geolocation is supported by this browser.');
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-    // handling the map click event
-    map.on('click', mapE => {
-        mapEvent = mapE;
+            navigator.geolocation.getCurrentPosition(
+                this._loadMap.bind(this),
+                error => {
+                    console.log('Could not get location by GPS: ', error.message);
+                })
+        };
+    }
+    _loadMap(position) {
+        // Get the position coordinates via GPS using the Geolocation API
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+        // Set the map with the coordinates
+        const coords = [latitude, longitude];
+
+        this.#map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+        // handling the map click event
+        this.#map.on('click', this._showForm.bind(this));
+    }
+    _pinMap(map, mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup'
+                })
+            )
+            .setPopupContent('Workout')
+            .openPopup();
+    }
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
         form.classList.remove('hidden');
         inputDistance.focus();
-        //pinMap(map, mapEvent);
-    });
+    }
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+    _newWorkout(e) {
+        e.preventDefault();
+        // clear the input fields
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
 
+        // display the form
+        this._pinMap(this.#map, this.#mapEvent);
+    }
 }
 
+const app = new maptyApp();
 
-if (navigator.geolocation) {
-    console.log('Geolocation is supported by this browser.');
-
-    navigator.geolocation.getCurrentPosition(
-        position => {
-            // Get the position coordinates via GPS using the Geolocation API
-            const latitude = position.coords;
-            const longitude = position.coords;
-            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-            // Set the map with the coordinates
-            renderMap(latitude, longitude);
-
-        }, error => {
-            console.log('Could not get location by GPS: ', error.message);
-            // https://www.google.com/maps/@23.4938181,120.4367907,15.79z
-
-            // Fallback to IP geolocation by using a third-party API
-            // https://ipapi.co/json/
-            fetch('https://ipapi.co/json/')
-                .then(response => response.json())
-                .then(locationData => {
-                    console.log(`City: ${locationData.city}, Lat: ${locationData.latitude}, Lon: ${locationData.longitude}`);
-                    renderMap(locationData.latitude, locationData.longitude);
-                })
-                .catch(error => console.error('IP Geolocation Error:', error));
-        })
-};
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    // clear the input fields
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
-
-    // display the form
-    pinMap(map, mapEvent);
-});
-
-inputType.addEventListener('change', function () {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
