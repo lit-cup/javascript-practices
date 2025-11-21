a66d31dd7e7d9729249f0c9155f4508c6b27556a
Chen Yi Ting <spu76611@gmail.com>
2025-11-21 08:16:29 +0800

    feat: add babel and Polyfill syntax

--- Basic/Perception/npmExample.js
+++ Basic/Perception/npmExample.js
@@ -43,3 +43,39 @@ console.log("Deep Clone:", stateDeepClone);
 if(module.hot){
     module.hot.accept();
 }
+
+// use babel and polyfill modern JS features for older browsers
+// goal: babel and polyfiling purpos let old browsers run modern JS code
+// when we write ES6+ code, the function might not be supported in older browsers
+// babel can transpile the code to ES5 and polyfill missing features
+
+// BABEL
+// babel basically future proof our code by transpiling modern JS syntax to older syntax
+// then automatically build step from using bundlers like Webpack or Parcel
+
+// POLYFILLING
+// polyfills like plugins or libraries that add missing features to older browsers
+
+
+// for example we can use core-js to polyfill features like Promise, Array.from, etc.
+import 'core-js/stable';
+
+// for async/await support in older browsers, we can use regenerator-runtime
+import 'regenerator-runtime/runtime';
+
+// self extra note:
+// 1. @babel/prest-env can automatically determine the necessary transformations and polyfills based on the target browser environments we specify
+
+// 2. why use regenerator-runtime?
+// because in old browsers that do not support async/await natively, babel transpiles async functions into generator functions
+
+// 3. when we are not use polyfill?
+{
+// 3-1. when we are targeting modern browsers that already support the features we are using
+// 3-2. when we use babel with prest-env usage option to only include necessary polyfills based on target browsers
+// 3-3. prest-env usage option include: "usage" | "entry" | "false"
+//      "usage": automatically add imports for polyfills based on the code we write
+//      "entry": we manually import core-js and regenerator-runtime at the entry point of our application
+//      "false": no polyfills are added, we handle it ourselves
+// 3-4. small demo, self project, course projects, etc. don't need polyfill for old browsers
+}
