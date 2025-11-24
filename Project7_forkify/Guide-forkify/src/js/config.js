ad8490a08ae55d504bafbe44760341a972dea75d
Chen Yi Ting <spu76611@gmail.com>
2025-11-24 23:22:28 +0800

    feat: add config.js

---
+++ Project7_forkify/Guide-forkify/src/js/config.js
@@ -0,0 +1,2 @@
+// put important data and could reuse and easy to change
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
--- Project7_forkify/Guide-forkify/src/js/model.js
+++ Project7_forkify/Guide-forkify/src/js/model.js
@@ -1,3 +1,5 @@
+import { API_URL } from "./config";
+
 export const state = {
     recipe: {},
 }
@@ -5,7 +7,7 @@ export const state = {
 // this model not will return anything keep it private
 export const loadRecipe = async function(id){
     try{
-        const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`); 
+        const res = await fetch(`${API_URL}/${id}`); 
         const data = await res.json();
         
         console.log(data);
