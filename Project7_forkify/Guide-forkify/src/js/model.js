633e4b1d7d8da1b918a7d8b1211334758185c768
Chen Yi Ting <spu76611@gmail.com>
2025-11-24 22:01:56 +0800

    Refactor: move to MVC design

---
+++ Project7_forkify/Guide-forkify/src/js/model.js
@@ -0,0 +1,33 @@
+export const state = {
+    recipe: {},
+}
+
+// this model not will return anything keep it private
+export const loadRecipe = async function(id){
+    try{
+        const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`); 
+        const data = await res.json();
+        
+        console.log(data);
+        // make better error message
+        if(!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+        console.log(res, data);
+
+        // set recipe object with porpoty we want
+        const { recipe } = data.data;
+        state.recipe = {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        sourceUrl: recipe.source_url,
+        image: recipe.image_url,
+        servings: recipe.servings,
+        cookingTime: recipe.cooking_time,
+        ingredients: recipe.ingredients,
+        }
+        console.log(state.recipe);
+    }catch(error){
+        alert(error);
+    }
+}
