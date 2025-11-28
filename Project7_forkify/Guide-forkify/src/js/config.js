d1ffe315227f5bd17c7044075b2bd058f54c527a
Chen Yi Ting <spu76611@gmail.com>
2025-11-28 23:36:42 +0800

    feat: add modal close duration constant to configuration

--- Project7_forkify/Guide-forkify/src/js/config.js
+++ Project7_forkify/Guide-forkify/src/js/config.js
@@ -3,3 +3,4 @@ export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
 export const API_KEY = '699f414f-36a8-404c-bbb9-87e201616079';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
