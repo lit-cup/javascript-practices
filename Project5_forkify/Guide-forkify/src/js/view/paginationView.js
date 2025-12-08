1c984017c1f0d6973a8b9605aafa7239381f1c62
Chen Yi Ting <spu76611@gmail.com>
2025-12-09 07:45:21 +0800

    TODO: add new feature display correct page between pagination button

--- Project5_forkify/Guide-forkify/src/js/view/paginationView.js
+++ Project5_forkify/Guide-forkify/src/js/view/paginationView.js
@@ -1,76 +1,86 @@
-import View from "./View.js";
+import View from './View.js';
 import icons from '../../img/icons.svg?url';
 
 class PaginationView extends View {
-    _parentElement = document.querySelector('.pagination');
+  _parentElement = document.querySelector('.pagination');
 
-    addHandlerPageButtonClick(controlPagination){
-        this._parentElement.addEventListener('click', function(e){
-            // use closest to find the button we click and listening
-            const btn = e.target.closest('.btn--inline');
-            if(!btn) return;
+  addHandlerPageButtonClick(controlPagination) {
+    this._parentElement.addEventListener('click', function (e) {
+      // use closest to find the button we click and listening
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
 
-            // get goto page from data-goto attribute
-            const goToPage = +btn.dataset.goto;
+      // get goto page from data-goto attribute
+      const goToPage = +btn.dataset.goto;
 
-            // call controlPagination function from controller.js, then use goToPage to render the right page we clikced
-            controlPagination(goToPage);
-        });
-    }
+      // call controlPagination function from controller.js, then use goToPage to render the right page we clikced
+      controlPagination(goToPage);
+    });
+  }
+  // TODO: display correct page between two pagination button
+  // use search data from model,use data-goto attribute to get which page to go
+  _generalMarkup() {
+    const curPage = this._data.page;
+    // calculate total number of pages
+    // returns the smallest integer greater than or equal to a given number.
+    const numPage = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
 
-    // use search data from model,use data-goto attribute to get which page to go
-    _generalMarkup(){
-        const curPage = this._data.page
-        // calculate total number of pages
-        // returns the smallest integer greater than or equal to a given number.
-        const numPage = Math.ceil(this._data.results.length / this._data.resultsPerPage);
-
-        // All page's situions for button
-        // Page1, and there are other pages
-        if(curPage === 1 && numPage > 1){
-            // return next button for render, for page 1
-            return `        
-            <button data-goto='${curPage + 1}' class="btn--inline pagination__btn--next">
+    // All page's situions for button
+    // Page1, and there are other pages
+    if (curPage === 1 && numPage > 1) {
+      // return next button for render, for page 1
+      return `        
+            <button data-goto='${
+              curPage + 1
+            }' class="btn--inline pagination__btn--next">
                 <span>Page ${curPage + 1}</span>
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-right"></use>
                 </svg>
             </button>`;
-        }
-        
-        // last page
-        if(curPage === numPage && numPage > 1){
-            // return pre button for render, for last page
-            return `
-            <button data-goto='${curPage - 1}' class="btn--inline pagination__btn--prev">
+    }
+
+    // last page
+    if (curPage === numPage && numPage > 1) {
+      // return pre button for render, for last page
+      return `
+            <button data-goto='${
+              curPage - 1
+            }' class="btn--inline pagination__btn--prev">
              <svg class="search__icon">
                <use href="${icons}#icon-arrow-left"></use>
              </svg>
              <span>Page ${curPage - 1}</span>
             </button>`;
-        }
-        
-        // other page
-        if(curPage < numPage){
-            // return prev/next button for render, for other page
-            return `
-            <button data-goto='${curPage - 1}' class="btn--inline pagination__btn--prev">
+    }
+
+    // other page
+    if (curPage < numPage) {
+      // return prev/next button for render, for other page
+      return `
+            <button data-goto='${
+              curPage - 1
+            }' class="btn--inline pagination__btn--prev">
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-left"></use>
                 </svg>
                 <span>Page ${curPage - 1}</span>
             </button>
-            <button data-goto='${curPage + 1}' class="btn--inline pagination__btn--next">
+            <button data-goto='${
+              curPage + 1
+            }' class="btn--inline pagination__btn--next">
                 <span>Page ${curPage + 1}</span>
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-right"></use>
                 </svg>
             </button>`;
-        }
-
-        // Page1, and there are no other pages
-        return '';
     }
+
+    // Page1, and there are no other pages
+    return '';
+  }
 }
 
 export default new PaginationView();
